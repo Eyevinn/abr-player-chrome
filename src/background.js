5481a6e2805f82b30843e287f7f2741407d3a38a
birme <jonas.birme@eyevinn.se>
2018-04-12 16:14:11 +0200

    Match 'manifest' as Smooth Streaming

--- src/background.js
+++ src/background.js
@@ -6,13 +6,13 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
 chrome.webRequest.onBeforeRequest.addListener(function(req) {
-  var re = new RegExp('(\.m3u8|\.mpd|Manifest)[\?]?.*');
+  var re = new RegExp('(\.m3u8|\.mpd|Manifest|manifest)[\?]?.*');
   if (req.url.match(re)) {
     return { redirectUrl: chrome.runtime.getURL('player.html') + "#" + req.url };
   } else {
     return null;
   }
 }, {
-  urls: [ "*://*/*.m3u8*", "*://*/*.mpd*", "*://*/*/Manifest" ],
+  urls: [ "*://*/*.m3u8*", "*://*/*.mpd*", "*://*/*/Manifest", "*://*/*/manifest" ],
   types: [ "main_frame" ]
 }, [ "blocking" ] );
